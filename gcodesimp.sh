bba8746058ab70538fe8fbde230b0aba19ee7cf1
D. Geerdink <100221343+DGDodo@users.noreply.github.com>
2026-02-03 02:17:53 +0100

    gcodesimp.sh
    Updated the shebang to use bash and added comments for clarity. Enhanced file handling and line counting logic.

--- gcodesimp.sh
+++ gcodesimp.sh
@@ -1,8 +1,8 @@
-#!/bin/sh
+#!/bin/bash
 # ========================================
 #         SCRIPT LINUXCNC SIMPLIFY
 # ========================================
-# feb 2026
+# feb 2026 DG
 #
 #;; Adjusted (LinuxCNC) gcode with simplified gcode.
 #;; Minimize movements, scriptly adjusted.
@@ -17,25 +17,81 @@
 #;;
 #;; Same for max: Y14.2500
 
+# Due to additional special characters (Windows) we have to translate some gcode.
+# https://stackoverflow.com/questions/25778587/identify-and-remove-specific-hidden-characters-from-text-file
+# Command: tr -cd '\11\12\40-\176' < org.gcode > new.gcode
+#
+
+# INIT
+# ====
 # Parameters
-X1=0
-X2=0
-Y1=0
-Y2=0
-Z1=0
-LC=0
+Pversion=1.0
+X1=0   # Lowest X-movement, higher than 0
+X2=0   # Highest X-movement
+Y1=0   # Lowest Y-movement, higher than 0
+Y2=0   # Highest Y-movement
+Z1=0   # Any Z movement
+LC=0   # Line count
 # Get program ID
 progid=$$
 
-# INIT
-if %1 ="" then 
-  echo "This program needs the .gcode file as parameter."
+# check $1 file
+if [ "$1" = "" ]; then
+  echo "This program needs the '~.gcode' file as parameter."
   exit
 fi
 
-# Program
-if [ -f %1 ] then
-  cp %1 /tmp/%1_simplified.gcode
+# PROGRAM
+# =======
+# copy original and always translate?
+if [ -f $1 ]; then
+  str=$(echo $1|cut -d"." -f1)
+  tr -cd '\11\12\40-\176' < $1 > "/tmp/"$str"_simplified.gcode"
+  str1="/tmp/"$str"_simplified.gcode"
 fi
+# grab nr of lines
+LC=$(wc -l $str1|cut -d" " -f1)
+
+#clear
+echo ""
+echo " LinuxCNC simplify script"
+echo "=========================="
+echo ""
+echo "Original file: '"$1"' has "$LC" lines."
+echo "File '"$str1"' has been created and will be adjusted."
+echo
+
+count1=0
+count2=0
+while read -r line; do
+  if [ -z "$line" ]; then  # Count if line is empty
+#    echo "Non-empty line: $line"
+    let count1++
+  else
+    if [ ${line:0:1} = ';' ]; then
+      let count2++
+    else                 # grab lowest and highest X, Y values
+      
+    fi
+  fi
+done < $str1
+
+echo "File has "$count1" empty lines."
+echo "File has "$count2" remark lines."
+#for /f "tokens=*" %%a in ($str1) do (
+#  echo line=%%a
+#)
+
+echo ""
+echo "minimum Y value: "
+echo "maximum Y value: "
+echo "minimum X value: "
+echo "maximum X value: "
+
+#echo "Has "$LC" lines."
 
 
+# END
+# ===
+echo ""
+exit
