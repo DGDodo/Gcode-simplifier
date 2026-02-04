b294c8bd2e6038588d826410ee42d99d430b5ec5
D. Geerdink <100221343+DGDodo@users.noreply.github.com>
2026-02-04 13:41:53 +0100

    Refactor gcodesimp.sh for clarity and efficiency
    Added comments for clarity and improved code structure. Enhanced the logic for handling temporary files and added functionality to remove unnecessary lines.

--- gcodesimp.sh
+++ gcodesimp.sh
@@ -17,12 +17,15 @@
 #;;
 #;; Same for max: Y14.2500 (example)
 
+# TO DO: remove end-lines (and start-lines) without Z movement
+
 # Due to additional special characters (Windows) we have to translate some gcode.
 # https://stackoverflow.com/questions/25778587/identify-and-remove-specific-hidden-characters-from-text-file
 # Command: tr -cd '\11\12\40-\176' < org.gcode > new.gcode
-#
+# Conversion is done on all loaded gcode.
 
-# We make 2 additional /tmp/~_temp files, one with X coordinates ans one with Y coordinates
+# We make 2 additional /tmp/~_temp files, one with X ands one with Y coordinates.
+# Files also used for conversion.
 
 # INIT
 # ====
@@ -45,44 +48,55 @@ fi
 
 # PROGRAM
 # =======
-# copy original and always translate?
+# copy and convert original gcode
 if [ -f $1 ]; then
-  str=$(echo $1|cut -d"." -f1)
+  str=$(echo $1|cut -d"." -f1)					# Filename till first point in filename
   tr -cd '\11\12\40-\176' < $1 > "/tmp/"$str"_simplified.gcode"
-  str1="/tmp/"$str"_simplified.gcode"
-  str2="/tmp/"$str"_Ytemp.txt"
-  str3="/tmp/"$str"_Xtemp.txt"
-  str21="/tmp/"$str"_Y2temp.txt"
-  str31="/tmp/"$str"_X2temp.txt"
+  str1="/tmp/"$str"_simplified.gcode"				# New simplified filename
+  str2="/tmp/"$str"_Ytemp.txt"					# Temporarly Y values 1 filename
+  str3="/tmp/"$str"_Xtemp.txt"					# Temporarly X values 1 filename
+  str21="/tmp/"$str"_Y2temp.txt"				# As above nr 2
+  str31="/tmp/"$str"_X2temp.txt"				# As above nr 2
 fi
+# Remove temporarly files if exist
+if [ -f $str1 ]; then rm $str1
+if [ -f $str2 ]; then rm $str2
+if [ -f $str3 ]; then rm $str3
+if [ -f $str21 ]; then rm $str21
+if [ -f $str31 ]; then rm $str31
+
 # grab nr of lines
 LC=$(wc -l $str1|cut -d" " -f1)
 
 #clear
-echo
+echo ""
 echo "=========================="
 echo " LinuxCNC simplify script"
 echo "=========================="
 echo ""
 echo "Original file: '"$1"' has "$LC" lines."
-echo "File '"$str1"' has been created and will be adjusted."
+echo "File '"$str1"' has been created and will be adjusted..."
+echo "Analyzing ..."
 echo
 
-count1=0
-count2=0
+count1=0  # Empty lines
+count2=0  # remarks / comments
+
 while read -r line; do
-  if [ -z "$line" ]; then                 # Count if line is empty
+  if [ -z "$line" ]; then                     # Count if line is empty
     let count1++
-  else                                    # If line is not empty
-    if [ ${line:0:1} = ';' ]; then        # If line is a remark / comment
+  else                                        # If line is not empty
+    if [ ${line:0:1} = ';' ]; then            # Count if line is a remark / comment
       let count2++
-    else                                  # grab X an Y values and put them into temp files
-      if [[ ! $line == *"G0"* ]]; then    # Omit gcode G0 lines
-        if [[ $line == *"Y"* ]]; then
-          echo $line|cut -d"Y" -f2|cut -d" " -f1 >> $str2
-        fi
-        if [[ $line == *"X"* ]]; then
-          echo $line|cut -d"X" -f2|cut -d" " -f1 >> $str3
+    else                                      # Grab X an Y values and put them into temp files
+      if [[ ! $line == *"G0"* ]]; then        # Omit gcode G0 lines    || [[ ! $line == *"G3"* ]]
+        if [[ ! $line == *"G3"* ]]; then      # Omit gcode G3 lines
+          if [[ $line == *"Y"* ]]; then
+            echo $line|cut -d"Y" -f2|cut -d" " -f1 >> $str2
+          fi
+          if [[ $line == *"X"* ]]; then
+            echo $line|cut -d"X" -f2|cut -d" " -f1 >> $str3
+          fi
         fi
       fi
     fi
@@ -105,12 +119,72 @@ Y2=$(tail -n 1 $str21)
 X1=$(head -n 1 $str31)
 X2=$(tail -n 1 $str31)
 
+echo
 echo "minimum Y value: "$Y1
 echo "maximum Y value: "$Y2
 echo "minimum X value: "$X1
 echo "maximum X value: "$X2
 
+echo
+echo  "Converting..."
+
+# First replace run: replace 'G1 Y<lowest> Z0.0000' with 'G1 Z0.0000 ;; removed Y<lowest>'
+# And the same for higest Y value
+string1="G1 Y"$Y1" Z0.0000"
+string2="G1 Z0.0000 ;; removed Y"$Y1
+string3="G1 Y"$Y2" Z0.0000"
+string4="G1 Z0.0000 ;; removed Y"$Y2
+string5=" Y"$Y1
+string6=";; Y"$Y1
+string7=" Y"$Y2
+string8=";; Y"$Y2
+string9=" Y"$Y1" Z0.0000"
+string10=" Z0.0000 ;; removed Y"$Y1
+string11=" Y"$Y2" Z0.0000"
+string12=" Z0.0000 ;; removed Y"$Y2
+
+while IFS='' read -r line; do
+  if [ "$line" = "$string1" ]; then echo "${line//$string1/$string2}"
+    else if [ "$line" = "$string3" ]; then echo "${line//$string3/$string4}"
+      else if [ "$line" = "$string5" ]; then echo "${line//$string5/$string6}"
+        else if [ "$line" = "$string7" ]; then echo "${line//$string7/$string8}"
+          else if [ "$line" = "$string9" ]; then echo "${line//$string9/$string10}"
+            else if [ "$line" = "$string11" ]; then echo "${line//$string11/$string12}"
+              else echo "$line"
+            fi
+          fi
+        fi
+      fi
+    fi
+  fi
+done < $str1 > $str2
+
+# Next find 'G1 ' rules for which the 'G1 ' has to be on the next line before ruling them out.
+
+string13="G1 Y"$Y1 # minimum Y value
+string14="G1 Y"$Y2 # maximum Y value
+
+while IFS='' read -r line; do
+  if [ "$line" = "$string13" ]; then
+    echo ";; "$line
+    read -r line
+    echo "G1 "$line
+    else if [ "$line" = "$string14" ]; then
+      echo ";; "$line
+      read -r line
+      echo "G1 "$line
+      else echo "$line"
+    fi
+  fi
+done < $str2 > $str3
+
+# TO DO: remove end-lines without Z movement
+
+# Copy end file to original location
+cp $str3 $str"_simplified.gcode"
+
 # END
 # ===
+echo "Done."
 echo
 exit
